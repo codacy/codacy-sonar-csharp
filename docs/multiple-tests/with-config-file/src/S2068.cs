74f99908f575c091ccba0988e8c256d758055607
Luís Ferreira <contact@lsferreira.net>
2019-11-21 18:12:40 +0000

    Add symbolic linkage instead of copying files
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- docs/multiple-tests/with-config-file/src/S2068.cs
+++ docs/multiple-tests/with-config-file/src/S2068.cs
@@ -1,9 +1,1 @@
-//#Patterns: S2068 : { "credentialWords": "passwor,passwords" }
-
-public class Foobar {
-    //#Error: S2068
-    public string passwor = "Password123";
-    //#Error: S2068
-    public string passwords = "Password123;Password321";
-    public string noPassword;
-}
+../../../tests/S2068.cs
