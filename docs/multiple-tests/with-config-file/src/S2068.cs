127f7f1dcc1bb028f7eda4e99a7ad72bb953a920
Luís Ferreira <contact@lsferreira.net>
2019-11-22 16:33:43 +0000

    Add parameters to patterns.xml and revert symbolic linkage
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- docs/multiple-tests/with-config-file/src/S2068.cs
+++ docs/multiple-tests/with-config-file/src/S2068.cs
@@ -1,1 +1,9 @@
-../../../tests/S2068.cs
+//#Patterns: S2068 : { "credentialWords": "passwor,passwords" }
+
+public class Foobar {
+    //#Error: S2068
+    public string passwor = "Password123";
+    //#Error: S2068
+    public string passwords = "Password123;Password321";
+    public string noPassword;
+}
--- docs/multiple-tests/without-config-file/src/S2223.cs
+++ docs/multiple-tests/without-config-file/src/S2223.cs
@@ -1,1 +1,20 @@
-../../../tests/S2223.cs
+//#Patterns: S2223
+
+public class ShapeBad
+{
+  //#Error: S2223
+  public static Shape Empty = new EmptyShape();  // Noncompliant
+
+  private class EmptyShape : Shape
+  {
+  }
+}
+
+public class ShapeGood
+{
+  public static readonly Shape Empty = new EmptyShape();
+
+  private class EmptyShape : Shape
+  {
+  }
+}
