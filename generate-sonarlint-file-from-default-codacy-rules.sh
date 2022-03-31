0064722d436b54a3d8a84ec63aeca5f425ed9eb1
Vasco Dias <vasco.dias@codacy.com>
2022-03-31 17:06:37 +0100

    create a script that generates a default SonarLint.xml with all default rules explicitly defined

---
+++ generate-sonarlint-file-from-default-codacy-rules.sh
@@ -0,0 +1,27 @@
+#!/bin/env bash
+
+container_id=$(docker create codacy-csharp:latest)
+codacy_rules_file=/tmp/codacy-rules.json
+
+# get all the rules defined for the tool from inside the docker container
+docker cp $container_id:/docs/patterns.json $codacy_rules_file
+docker rm $container_id
+
+# obtain all ids of the rules we have enabled by default
+rules_ids=$(cat $codacy_rules_file | jq -r '.patterns[] | select (.enabled == false) | .patternId')
+
+cat << EOF > SonarLint.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<AnalysisInput>
+<Rules>
+EOF
+
+for rule_id in $rules_ids
+do
+	echo -e "\t<Rule>\n\t\t<Key>$rule_id</Key>\n\t</Rule>\n" >> SonarLint.xml
+done
+
+cat << EOF >> SonarLint.xml
+</Rules>
+</AnalysisInput>
+EOF
