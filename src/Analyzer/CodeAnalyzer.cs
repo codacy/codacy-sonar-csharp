f97c828eae249135f0d1386731420b70e639b4d9
DMarinhoCodacy <102817200+DMarinhoCodacy@users.noreply.github.com>
2024-10-22 17:28:05 +0100

    improve codeanalyzer (#249)
    * improve codeanalyzer

--- src/Analyzer/CodeAnalyzer.cs
+++ src/Analyzer/CodeAnalyzer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.IO;
@@ -24,11 +25,11 @@ namespace CodacyCSharp.Analyzer
         private readonly string sonarConfigurationPath;
         private readonly ImmutableArray<DiagnosticAnalyzer> availableAnalyzers;
         private readonly DiagnosticsRunner diagnosticsRunner;
-
         private readonly string tmpSonarLintFolder;
-        private static List<string> blacklist = new List<string> { "S1144", "S2325", "S2077"};
+        private static HashSet<string> blacklist = new HashSet<string> { "S1144", "S2325", "S2077" };
 
-        public static bool IsInBlacklist(string id) {
+        public static bool IsInBlacklist(string id)
+        {
             return blacklist.Contains(id);
         }
 
@@ -38,19 +39,21 @@ namespace CodacyCSharp.Analyzer
             availableAnalyzers = ImmutableArray.Create(
                 new RuleFinder()
                     .GetAnalyzerTypes()
-                    .Select(type => (DiagnosticAnalyzer) Activator.CreateInstance(type))
+                    .Select(type => (DiagnosticAnalyzer)Activator.CreateInstance(type))
                     .ToArray());
 
             var additionalFiles = new List<AnalyzerAdditionalFile>();
 
             if (!(PatternIds is null) && PatternIds.Any())
             {
-                // create temporary directory
+                // create temporary directory only if needed
                 this.tmpSonarLintFolder = Path.Combine(Path.GetTempPath(), "sonarlint_" + Guid.NewGuid());
                 Directory.CreateDirectory(tmpSonarLintFolder);
                 var tmpSonarLintPath = Path.Combine(tmpSonarLintFolder, defaultSonarConfiguration);
+
                 var rules = new XElement("Rules");
                 var patterns = CurrentTool.Patterns.Where(pattern => !IsInBlacklist(pattern.PatternId)).ToArray();
+
                 foreach (var pattern in patterns)
                 {
                     var parameters = new XElement("Parameters");
@@ -69,20 +72,17 @@ namespace CodacyCSharp.Analyzer
 
                 new XDocument(new XElement("AnalysisInput", rules)).Save(tmpSonarLintPath);
                 additionalFiles.Add(new AnalyzerAdditionalFile(tmpSonarLintPath));
-            } else if (File.Exists(sonarConfigurationPath)) {
-                additionalFiles.Add(new AnalyzerAdditionalFile(sonarConfigurationPath));
             }
-
-            // if we don't have patterns let's get them from the config file
-            if (PatternIds is null && File.Exists(sonarConfigurationPath))
+            else if (File.Exists(sonarConfigurationPath))
             {
-                var xmlDoc = XDocument.Load(sonarConfigurationPath);
-                var analysisInput = xmlDoc.Element("AnalysisInput");
-                var rules = analysisInput.Element("Rules");
+                var configurationFile = XDocument.Load(sonarConfigurationPath).Element("AnalysisInput");
+                additionalFiles.Add(new AnalyzerAdditionalFile(sonarConfigurationPath));
+                
+                // Load patterns from cached configuration file
+                var rules = configurationFile.Element("Rules");
                 if (rules != null)
                 {
-                    PatternIds = rules.Elements("Rule").Select(e => e.Elements("Key").Single().Value)
-                        .ToImmutableList();
+                    PatternIds = rules.Elements("Rule").Select(e => e.Elements("Key").Single().Value).ToImmutableList();
                 }
             }
 
@@ -95,9 +95,6 @@ namespace CodacyCSharp.Analyzer
             GC.SuppressFinalize(this);
         }
 
-        /// <summary>
-        ///     Free temporary resources created on object construction.
-        /// </summary>
         ~CodeAnalyzer()
         {
             Dispose(false);
@@ -105,7 +102,6 @@ namespace CodacyCSharp.Analyzer
 
         protected virtual void Dispose(bool disposing)
         {
-            // delete created temporary directory
             if (tmpSonarLintFolder != null)
             {
                 Directory.Delete(tmpSonarLintFolder, true);
@@ -114,19 +110,12 @@ namespace CodacyCSharp.Analyzer
 
         protected override async Task Analyze(CancellationToken cancellationToken)
         {
-            foreach (var file in Config.Files) {
-                // skip all files that don't have the main language extension
-                // To allow Semgrep to run on C# configuration files
-                // We are analyzing `Directory.Packages.props` as if it was a C# file
-                // This is to avoid that.
-                // More info here: https://codacy.zendesk.com/agent/tickets/44462
-                if (!file.EndsWith(".cs"))
-                {
-                    continue;
-                }
+            // Process files concurrently for better performance
+            var fileAnalysisTasks = Config.Files
+                .Where(file => file.EndsWith(".cs")) // only analyze C# files
+                .Select(file => Analyze(file, cancellationToken));
 
-                await Analyze(file, cancellationToken).ConfigureAwait(false);
-            }
+            await Task.WhenAll(fileAnalysisTasks);
         }
 
         public async Task Analyze(string file, CancellationToken cancellationToken)
@@ -136,28 +125,24 @@ namespace CodacyCSharp.Analyzer
                 var solution = CompilationHelper.GetSolutionFromFile(DefaultSourceFolder + file);
                 var compilation = await solution.Projects.First().GetCompilationAsync();
 
-                foreach (var diagnostic in await diagnosticsRunner.GetDiagnostics(compilation, cancellationToken))
+                // Parallelize diagnostics fetching
+                var diagnostics = await diagnosticsRunner.GetDiagnostics(compilation, cancellationToken);
+
+                Parallel.ForEach(diagnostics, diagnostic =>
                 {
                     var result = new CodacyResult
                     {
                         Filename = file,
                         PatternId = diagnostic.Id,
-                        Message = diagnostic.GetMessage()
+                        Message = diagnostic.GetMessage(),
+                        Line = diagnostic.Location != Location.None ? diagnostic.Location.GetLineNumberToReport() : 1
                     };
 
-                    if (diagnostic.Location != Location.None)
+                    if (!IsInBlacklist(diagnostic.Id))
                     {
-                        result.Line = diagnostic.Location.GetLineNumberToReport();
-                    }
-                    else
-                    {
-                        result.Line = 1;
-                    }
-
-                    if(!IsInBlacklist(diagnostic.Id)) {
                         Console.WriteLine(result);
                     }
-                }
+                });
             }
             catch (Exception e)
             {
@@ -177,27 +162,26 @@ namespace CodacyCSharp.Analyzer
             {
                 return availableAnalyzers;
             }
-            var builder = ImmutableArray.CreateBuilder<DiagnosticAnalyzer>();
 
-            foreach (var analyzer in availableAnalyzers
-                .Where(analyzer => analyzer.SupportedDiagnostics.Any(diagnostic => PatternIds.Contains(diagnostic.Id))))
-                builder.Add(analyzer);
+            // Initialize only relevant analyzers
+            var relevantAnalyzers = availableAnalyzers
+                .Where(analyzer => analyzer.SupportedDiagnostics.Any(diagnostic => PatternIds.Contains(diagnostic.Id)))
+                .ToImmutableArray();
 
-            return builder.ToImmutable();
+            return relevantAnalyzers;
         }
 
         public static ImmutableArray<DiagnosticAnalyzer> GetUtilityAnalyzers()
         {
-            var builder = ImmutableArray.CreateBuilder<DiagnosticAnalyzer>();
             var utilityAnalyzerTypes = RuleFinder.GetUtilityAnalyzerTypes()
                 .Where(t => !t.IsAbstract)
                 .ToList();
 
-            foreach (var analyzer in utilityAnalyzerTypes
-                .Select(type => (DiagnosticAnalyzer) Activator.CreateInstance(type)))
-                builder.Add(analyzer);
+            var utilityAnalyzers = utilityAnalyzerTypes
+                .Select(type => (DiagnosticAnalyzer)Activator.CreateInstance(type))
+                .ToImmutableArray();
 
-            return builder.ToImmutable();
+            return utilityAnalyzers;
         }
     }
 }
